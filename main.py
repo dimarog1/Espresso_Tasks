4f5e9149a5335b40d54b689dc0cadcf1194879f9
dimarog1 <76162370+dimarog1@users.noreply.github.com>
2021-11-22 03:00:34 +0300

    Задача №5 изменение дб

--- main.py
+++ main.py
@@ -6,15 +6,19 @@ from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
 
 
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("main.ui", self)
+        uic.loadUi("addEditCoffeeForm.ui", self)
         self.con = sqlite3.connect("coffee.db")
-        self.tableWidget.itemChanged.connect(self.item_changed)
-        self.modified = {}
+        self.modified = []
         self.titles = None
         self.load_table()
+        self.tableWidget.itemChanged.connect(self.item_changed)
 
     def load_table(self):
         cur = self.con.cursor()
@@ -23,21 +27,41 @@ class MyWidget(QMainWindow):
         if not result:
             return
         self.tableWidget.setColumnCount(len(result[0]))
-        self.titles = [description[0] for description in cur.description]
+        self.titles = ['id', 'name', 'sort', 'roast_degree', 'type', 'flavour_description', 'price', 'packing_volume']
         for i, elem in enumerate(result):
             for j, val in enumerate(elem):
                 self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
-        self.modified = {}
+        self.modified = []
 
     def item_changed(self, item):
-        pass
+        print(self.titles)
+        mod = f'{self.titles[item.column()]} = "{item.text()}"'
+        changes = f'id = "{self.tableWidget.item(item.row(), 0).text()}"'
+        print(mod)
+        print(changes)
+        self.modified.append(changes)
+        self.modified.append(mod)
+        self.save_results()
 
     def save_results(self):
-        pass
+        if self.modified:
+            cur = self.con.cursor()
+            que = "UPDATE coffee\nSET "
+            que += self.modified[1] + '\n'
+            que += f'WHERE {self.modified[0]}'
+            print(que)
+            try:
+                cur.execute(que)
+                self.con.commit()
+                self.modified = []
+            except Exception as e:
+                print(e)
+                exit()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyWidget()
     ex.show()
+    # sys.excepthook = except_hook
     sys.exit(app.exec())
